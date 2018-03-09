d949f76cf9e2720ee9ccbb81757ad692e92cc3e0
thocode88 <37206307+thocode88@users.noreply.github.com>
2018-03-10 02:54:27 +0700

    Update pemanggil.js

--- pemanggil.js
+++ pemanggil.js
@@ -19,11 +19,10 @@ var a_to_ve="";var a_to_vf=new Array();var a_to_vg=0;a_to_ve=document.getElement
 j++;}
 if(a_to_vh==false)
 {var encryptedUrl=Base64.encode(a_to_vi);
-var x=Math.floor((Math.random()*3)+ 1);
+var x=Math.floor((Math.random()*2)+ 1);
 var xxx=null;
-if(x=="1"){xxx="/2018/03/being-smart-about-choosing-your-health.html"}
-if(x=="2"){xxx="/2018/03/given-high-cost-of-major-medical.html"}
-if(x=="3"){xxx="/2018/03/being-smart-about-choosing-your-health.html"}a_to_ve[i].href="https://jikaini.blogspot.com"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="1"){xxx="/2018/03/given-high-cost-of-major-medical.html"}
+if(x=="2"){xxx="/2018/03/being-smart-about-choosing-your-health.html"}a_to_ve[i].href="https://jikaini.blogspot.com"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
